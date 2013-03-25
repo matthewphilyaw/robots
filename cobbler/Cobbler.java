25af7a0df5170518391c93ce23ba6f8ebe62d70b
Matthew Philyaw <matthew.philyaw@gmail.com>
2013-03-25 02:19:46 -0400

    fixed tab issue.

--- cobbler/Cobbler.java
+++ cobbler/Cobbler.java
@@ -4,39 +4,39 @@ import robocode.*;
 public class Cobbler extends Robot
 {
     private int timesBeforeFire = 0;
-	public void run() {
-		while (true) {
-			this.turnGunRight(360);
-			this.ahead(100);
-		}
-	}
+    public void run() {
+        while (true) {
+            this.turnGunRight(360);
+            this.ahead(100);
+        }
+    }
 
-	public void onScannedRobot(ScannedRobotEvent e) {
-		// should never be less than zero, but it's an old habbit.
-		if (this.getGunHeat() <= 0) {
-			this.fire(1);
+    public void onScannedRobot(ScannedRobotEvent e) {
+        // should never be less than zero, but it's an old habbit.
+        if (this.getGunHeat() <= 0) {
+            this.fire(1);
             System.out.println("calls before fire: " + Integer.toString(timesBeforeFire));
             timesBeforeFire = 0;
         }
         else
             timesBeforeFire++;
-	}
+    }
 
-	public void onHitWall(HitWallEvent e) {
-		// square up to the wall
-		this.turnRight(e.getBearing());
-		// turn the fuck around with mad tank face
-		this.turnRight(180);
-		this.turnGunRight(360);
-		this.ahead(50);
-		this.turnRight(90);
-	}
+    public void onHitWall(HitWallEvent e) {
+        // square up to the wall
+        this.turnRight(e.getBearing());
+        // turn the fuck around with mad tank face
+        this.turnRight(180);
+        this.turnGunRight(360);
+        this.ahead(50);
+        this.turnRight(90);
+    }
 
-	public void onHitRobot(HitRobotEvent e) {
-		if (e.getBearing() > -90 && e.getBearing() <= 90) {
-			this.back(100);
-			this.turnRight(-90);
-		}
-	}
+    public void onHitRobot(HitRobotEvent e) {
+        if (e.getBearing() > -90 && e.getBearing() <= 90) {
+            this.back(100);
+            this.turnRight(-90);
+        }
+    }
 }
-											
+                                            
