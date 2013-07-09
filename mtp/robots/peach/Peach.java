b292e19988103cd05605935640dcd39a69468075
matthew philyaw <matthew.philyaw@gmail.com>
2013-07-09 01:23:42 -0400

    refactored the targeting portion and cleaned it up.

--- mtp/robots/peach/Peach.java
+++ mtp/robots/peach/Peach.java
@@ -1,12 +1,14 @@
 package mtp.robots.peach;
 import robocode.*;
-import java.util.*;
 import java.awt.Graphics2D;
 
 public class Peach extends AdvancedRobot {
     private TargetingData td;
 
     public void run() {
+        this.setAdjustGunForRobotTurn(true);
+        this.setAdjustRadarForGunTurn(true);
+
         while (true) {
             waitFor(new RadarTurnCompleteCondition(this));
             setTurnRadarRight(360);
@@ -18,52 +20,14 @@ public class Peach extends AdvancedRobot {
     public void onHitWall(HitWallEvent e) {
         this.turnRight(e.getBearing());
         this.turnRight(180);
-        this.turnGunRight(360);
         this.ahead(50);
         this.turnRight(90);
     }
 
-    // abstract further.
-    // Create interface for target
-    // use anonymous inner classes to generate one based off event
     public void onScannedRobot(ScannedRobotEvent e) {
         try {
-            final double angle = Math.toRadians((this.getHeading() + e.getBearing()) % 360);
-            final Point target =  new Point((int)(getX() + Math.sin(angle) * e.getDistance()),
-                                            (int)(getY() + Math.cos(angle) * e.getDistance()));
-            final Point assassin = new Point((int)getX(), (int)getY());
-
-            final int tv = (int)e.getVelocity();
-            final int av = (int)this.getVelocity();
-
-            td = new TargetingData(this.getTime());
-            List<TargetingPrediction> pd = td.getPredictions();
-
-            //pd.add(new TargetingPrediction(target, assassin, 0));
-
-            final double ta = Math.toRadians((e.getHeading() % 360));
-            final double aa = Math.toRadians((this.getHeading() % 360));
-
-            for (int t = 1; t < 25; t++) {
-                final int th = tv * (t);
-                final int ah = av * (t);
-
-                final Point targetP = new Point ((int)(Math.sin(ta) * th) + target.getX(),
-                                           (int)(Math.cos(ta) * th) + target.getY());
-                final Point assassinP = new Point ((int)(Math.sin(aa) * ah) + assassin.getX(),
-                                             (int)(Math.cos(aa) * ah) + assassin.getY());
-
-                TargetingPrediction tp = new TargetingPrediction(targetP, assassinP, t);
-                out.println("----------------------");
-                out.format("Tick: %d\nAngle needed: %f\nVelocity needed: %f\nDistance to target: %f\n",
-                            tp.getTime(),
-                            tp.getAngle(),
-                            tp.getVelocity(),
-                            tp.getDistance());
-                pd.add(tp);
-
-            }
-            out.println("=============================");
+            td =  TargetingData.GenerateTargetingData(this, e, 5);
+            out.println(td);
         }
         catch (Exception ex) {
             out.println(ex.getMessage());
@@ -74,5 +38,7 @@ public class Peach extends AdvancedRobot {
         if (td == null) return;
         if (this.getTime() - td.getTimeCreated() > 5) { return; }
         td.renderPredictions(g);
+
+
     }
 }
--- mtp/robots/peach/TargetingData.java
+++ mtp/robots/peach/TargetingData.java
@@ -1,5 +1,8 @@
 package mtp.robots.peach;
 
+import robocode.AdvancedRobot;
+import robocode.ScannedRobotEvent;
+
 import java.awt.*;
 import java.util.*;
 import java.util.List;
@@ -7,12 +10,53 @@ import java.util.List;
 public class TargetingData {
     final List<TargetingPrediction> predictions = new ArrayList<TargetingPrediction>();
     final long timeCreated;
+    final AdvancedRobot self;
 
-    public TargetingData(long timeCreated) {
-       this.timeCreated = timeCreated;
+    private TargetingData(long timeCreated, AdvancedRobot self) {
+        this.timeCreated = timeCreated;
+        this.self = self;
     }
+
+    public static TargetingData GenerateTargetingData(AdvancedRobot self, ScannedRobotEvent e, int numPredictions) {
+        // angle between my current heading and the bearing line to the target
+        final double angle = self.getHeadingRadians() + e.getBearingRadians();
+
+        // targets current point on the grid
+        final int targetVel = (int)e.getVelocity();
+        final Point targetPos =  new Point((int)(self.getX() + Math.sin(angle) * e.getDistance()),
+                                           (int)(self.getY() + Math.cos(angle) * e.getDistance()));
+
+        // my current point on the grid
+        final int assassinVel = (int)self.getVelocity();
+        final Point assassinPos = new Point((int)self.getX(), (int)self.getY());
+
+        TargetingData td = new TargetingData(self.getTime(), self);
+        List<TargetingPrediction> pd = td.getPredictions();
+
+        for (int t = 1; t <= numPredictions; t++) {
+            final int th = targetVel * (t);
+            final int ah = assassinVel * (t);
+
+            final Point targetP = new Point ((int)(Math.sin(e.getHeadingRadians()) * th) + targetPos.getX(),
+                                             (int)(Math.cos(e.getHeadingRadians()) * th) + targetPos.getY());
+            final Point assassinP = new Point ((int)(Math.sin(self.getHeadingRadians()) * ah) + assassinPos.getX(),
+                                               (int)(Math.cos(self.getHeadingRadians()) * ah) + assassinPos.getY());
+
+            TargetingPrediction tp = new TargetingPrediction(targetP, assassinP, t);
+            pd.add(tp);
+        }
+
+        return td;
+    }
+
+    public void getTargetingSolutions() {
+
+    }
+
     public List<TargetingPrediction> getPredictions() { return this.predictions; }
+
     public long getTimeCreated() { return this.timeCreated; }
+
     public void renderPredictions(Graphics2D g) {
         g.setColor(new Color(0, 255, 0, 255));
         for (TargetingPrediction t : this.predictions) {
@@ -20,5 +64,23 @@ public class TargetingData {
             g.fillOval(t.getAssassin().getX() - 2, t.getAssassin().getY() - 2, 4, 4);
             g.fillOval(t.getTarget().getX() - 5, t.getTarget().getY() - 5, 10, 10);
         }
+    }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("== == == ==\n");
+        for (TargetingPrediction t : this.predictions)
+        {
+            sb.append("-- -- -- --\n");
+            sb.append(String.format("Tick: %d\nAngle needed: %f\nVelocity needed: %f\nDistance to target: %f\n",
+                      t.getTime(),
+                      t.getAngle(),
+                      t.getVelocity(),
+                      t.getDistance()));
+        }
+        sb.append("== == == ==\n");
+
+        return sb.toString();
     }
 }
